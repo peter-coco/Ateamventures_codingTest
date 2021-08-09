8a2bef27050680e704b080e9ef2bcbc3c1fa6898
coco-peter <dklirser@gmail.com>
2021-08-09 14:15:12 +0900

    Main-Filtering : HTML, CSS

--- src/components/main/mainRequestLists.tsx
+++ src/components/main/mainRequestLists.tsx
@@ -2,6 +2,13 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 
 const MainRequestListsWrap = styled.div``;
+const MainRequestListsTop = styled.div``;
+const MainRequestListsTopTitle = styled.div``;
+const MainRequestListsTopSubscription = styled.div``;
+const MainRequestListsTopDuedate = styled.div``;
+const MainRequestListsDivider = styled.div``;
+const MainRequestListsBottom = styled.div``;
+const MainRequestListsBottom = styled.div``;
 
 const MainRequestLists = () => {
   return <MainRequestListsWrap></MainRequestListsWrap>;
